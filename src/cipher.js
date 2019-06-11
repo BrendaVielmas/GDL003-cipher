6e0775196f74940a13bc9a06ae22cb44faf7fe09
Brenda Vielmas <breen.vielmas@gmail.com>
2019-06-11 00:02:27 -0500

    modificación cipher sin DOM

--- src/cipher.js
+++ src/cipher.js
@@ -1,70 +1,62 @@
 window.cipher = {
-//Transforma el mensaje de un string a un número para poder realizar las fórmulas y lo regresa a ser letra
-//function cipherMessage() {
-cipherMessage : () => {
-  messageOutArea.style.display ="block";
-  let message = messageInput.value;
-  messageOutput.innerHTML = "";
-  let newLetter = "";
+  //Pedazo de Cifrado
+  encode : (offset, message) => {
+    let newLetter = "";
     /*Inicio: iteración en 0, condición(se cumple o no), codigo(resultado) si cumple->incremento, 
     se realiza de nuevo la condición, codigo(resultado) si cumple->incremento, etc.*/
-  for(i=0;i <= message.length-1; i++) {
-     //Letra actual es igual al mensaje iterado
-     let currentLetter = message.charCodeAt(i);
-      /*El numero de la letra con cipher es igual al numero de la letra más el desplazamiento 
-     agregado. Si es mayor a 256 el desplazamiento, se suma el residuo de 256*/
-     let numberWithCipher = (currentLetter + parseInt(offsetInput.value)) % 256;
-     /*La nueva letra... 1. Si el numero de la letra con cipher es mayor o igual a 0, es igual a valor de la letra
-     por el numero de la letra con cipher.
-     2. Si no se cumple 1., la nueva letra es igual a valor de la letra por el numero de la letra con cipher. 
-     PERO el numero de la letra con cipher es igual a 256 menos (el numero de la letra menos
-      el desplazamiento) si es mayor a 256 se resta el residuo de 256.
-     El "256-" hace que cifre en número negativo*/
-    if (numberWithCipher >= 0) {
-    newLetter += String.fromCharCode(numberWithCipher);
-    } else {
-    numberWithCipher = 256 - (currentLetter - parseInt(offsetInput.value)) % 256;
-    newLetter += String.fromCharCode(numberWithCipher);
-    }
-    //Arroja el mensaje 
-    messageOutput.innerHTML = newLetter;
-    //Esto es sólo para ver qué arroja la consola
-    console.log (currentLetter + " " + numberWithCipher + " " + newLetter);
-  }
-},
-
-//Transforma el mensaje de un string a un número para poder realizar las fórmulas y lo regresa a ser letra
-//function decipherMessage2() {
-decipherMessage2 : () => {
-  messageOutArea2.style.display ="block";
-  let message2 = messageInput2.value;
-  messageOutput2.innerHTML = "";
-  let newLetter2 = "";
-    /*Inicio: iteración en 0, condición(se cumple o no), codigo(resultado) si cumple->incremento, 
-    se realiza de nuevo la condición, codigo(resultado) si cumple->incremento, etc.*/
-    for(i=0;i <= message2.length-1;i++) {
+    let encodedMessage ="";
+    for(let i=0;i <= message.length-1; i++) {
       //Letra actual es igual al mensaje iterado
-      let currentLetter2 = message2[i].charCodeAt(0);
+      let currentLetter = message.charCodeAt(i);
       /*El numero de la letra con cipher es igual al numero de la letra más el desplazamiento 
       agregado. Si es mayor a 256 el desplazamiento, se suma el residuo de 256*/
-      let numberWithCipher2 = (currentLetter2 - parseInt(offsetInput2.value)) % -256;
-       /*La nueva letra... 1. Si el numero de la letra con cipher es mayor o igual a 0, es igual a valor de la letra
-       por el numero de la letra con cipher.
-       2. Si no se cumple 1., la nueva letra es igual a valor de la letra por el numero de la letra con cipher. 
-      PERO el numero de la letra con cipher es igual a 256 menos (el numero de la letra menos
-      el desplazamiento) si es mayor a 256 se resta el residuo de 256. 
+      let numberWithCipher = (currentLetter + offset) % 256;
+      /*Si numero de la letra con desplazamiento es mayor o igual a 0, la nueva letra  es igual a devolver el 
+      numero a caracter; Si no, la nueva letra  es igual a devolver el 
+      numero a caracter, PERO el numero de la letra con desplazamiento  es igual a 256 -(letra actual - desplazamiento) % 256
       El "256-" hace que cifre en número negativo*/
-      if (numberWithCipher2 >= 0) {
-      newLetter2 += String.fromCharCode(numberWithCipher2);
+      if (numberWithCipher >= 0) {
+        newLetter = String.fromCharCode(numberWithCipher);
       } else {
-      numberWithCipher2 = 256 + (currentLetter2 - parseInt(offsetInput2.value)) % 256;
-      newLetter2 += String.fromCharCode(numberWithCipher2);
+        numberWithCipher = 256 - (currentLetter - offset) % 256;
+        newLetter = String.fromCharCode(numberWithCipher);
       }
-   
-    //Arroja el mensaje 
-    messageOutput2.innerHTML = newLetter2;
-    //Esto es sólo para ver qué arroja la consola
-    console.log (currentLetter2 + " " + numberWithCipher2 + " " + newLetter2);
-  }
+      //Arroja el mensaje 
+      encodedMessage += newLetter;
+      //Esto es sólo para ver qué arroja la consola
+      console.log (currentLetter + " " + numberWithCipher + " " + newLetter);
+    }
+    return encodedMessage;
+  },
+
+
+  //Pedazo de Decifrado
+  decode : (offset, message) => {
+    let newLetter2 = "";
+    /*Inicio: iteración en 0, condición(se cumple o no), codigo(resultado) si cumple->incremento, 
+    se realiza de nuevo la condición, codigo(resultado) si cumple->incremento, etc.*/
+    let decodedMessage ="";
+    for(let i=0;i <= message.length-1;i++) {
+      //Letra actual es igual al mensaje iterado
+      let currentLetter2 = message.charCodeAt(i);
+      /*El numero de la letra con cipher es igual al numero de la letra más el desplazamiento 
+      agregado. Si es mayor a 256 el desplazamiento, se suma el residuo de 256*/
+      let numberWithCipher2 = (currentLetter2 - offset) % -256;
+       /*Si numero de la letra con desplazamiento es mayor o igual a 0, la nueva letra  es igual a devolver el 
+      numero a caracter; Si no, la nueva letra  es igual a devolver el 
+      numero a caracter, PERO el numero de la letra con desplazamiento  es igual a 256 +(letra actual - desplazamiento) % 256
+      El "256+" hace que cifre en número negativo*/
+      if (numberWithCipher2 >= 0) {
+        newLetter2 = String.fromCharCode(numberWithCipher2);
+      } else {
+        numberWithCipher2 = 256 + (currentLetter2 - offset) % 256;
+        newLetter2 = String.fromCharCode(numberWithCipher2);
+      }
+      //Arroja el mensaje 
+      decodedMessage += newLetter2;
+      //Esto es sólo para ver qué arroja la consola
+      console.log (currentLetter2 + " " + numberWithCipher2 + " " + newLetter2);
+    }
+    return decodedMessage;
   }
 };
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+//oculta elementos
 document.getElementById("messageSection").style.display = "none";
 document.getElementById("cipherButton2").style.display = "none";
 document.getElementById("decipherButton2").style.display = "none";
@@ -8,32 +9,36 @@ document.getElementById("messageOutArea2").style.display = "none";
 document.getElementById("messageOutArea").style.display = "none";
 
 
-
-
-
 //Funciones de ventana de Cifrado
+//Transforma el mensaje de un string a un número para poder realizar las fórmulas y lo regresa a ser letra
+let encode = () => {
+  document.getElementById("messageOutArea").style.display ="block";
+  let message = document.getElementById("messageInput").value;
+  let offset = parseInt(document.getElementById("offsetInput").value);
+  let encodedMessage = window.cipher.encode(offset, message);
+  //Arroja el mensaje 
+   document.getElementById("messageOutput").innerHTML = encodedMessage;
+};
 //Muestra la primer ventana donde está la opción de cifrar o descifrar
-
-//function showCipherWindow(){
-let decipherMessage2 = () => {
-  window.cipher.decipherMessage2();
-};
-let cipherMessage = () => {
-  window.cipher.cipherMessage();
-};
-
 let showCipherWindow = () => {
   document.getElementById("accessButtonsSection").style.display = "none";
   document.getElementById("messageSection").style.display = "block";
   document.getElementById("cipherButton").style.display = "block";
   document.getElementById("offsetNumber").style.display = "block";
   document.getElementById("cipherButton2").style.display = "block";
-
 };
 
-/*Funciones de ventana de Descifrado
-Muestra la primer ventana donde está la opción de cifrar o descifrar*/
-//function showDecipherWindow(){
+
+//Funciones de ventana de Descifrado
+let decode = () => {
+  document.getElementById("messageOutArea2").style.display ="block";
+  let message = document.getElementById("messageInput2").value;
+  let offset = parseInt(document.getElementById("offsetInput2").value);
+  let decodedMessage = window.cipher.decode(offset, message);
+  //Arroja el mensaje 
+  document.getElementById("messageOutput2").innerHTML = decodedMessage;
+};
+//Muestra la primer ventana donde está la opción de cifrar o descifrar
 let showDecipherWindow = () => {
   document.getElementById("accessButtonsSection").style.display = "none";
   document.getElementById("decipherButton").style.display = "block";
@@ -43,9 +48,9 @@ let showDecipherWindow = () => {
 };
 
 document.getElementById("cipherButton").addEventListener("click",showCipherWindow);
-document.getElementById("decipherButton2").addEventListener("click",decipherMessage2);
-document.getElementById("cipherButton2").addEventListener("click",cipherMessage);
+document.getElementById("decipherButton2").addEventListener("click",decode);
+document.getElementById("cipherButton2").addEventListener("click",encode);
 /*document.getElementById("cipherButton2").addEventListener("click",() => {
-  innerHTML = window.cipher.cipherMessage();
+  innerHTML = window.cipher.encode();
 })); */
 document.getElementById("decipherButton").addEventListener("click",showDecipherWindow);
